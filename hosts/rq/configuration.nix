a0495ba8651875c28678bdc93933d5bb05fd3c29
Jake Ginesin <jakeginesin@gmail.com>
2026-01-30 02:35:47 -0500

    generation 572 25.11.20250714.62e0f05

--- hosts/rq/configuration.nix
+++ hosts/rq/configuration.nix
@@ -21,6 +21,8 @@
       users.synchronous.imports = [../../home/home.nix];
     };
 
+    services.udev.packages = [pkgs.android-udev-rules];
+
     boot.loader.grub.enable = true;
     boot.loader.grub.device = "/dev/nvme0n1";
     boot.loader.grub.useOSProber = true;
