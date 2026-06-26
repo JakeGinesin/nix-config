a5a76e38fd6b0ede7d8ec6f356e2ce3092f2abc2
JakeGinesin <jakeginesin@gmail.com>
2026-06-25 22:32:53 -0400

    generation 34 25.11.20250714.62e0f05

--- home/scripts/backup-journal.sh
+++ home/scripts/backup-journal.sh
@@ -1,5 +1,8 @@
-restic -r sftp:${aliases[kubeserver]#ssh }:/home/synchronous/backups/journal backup /home/synchronous/journal
+#!/usr/bin/env zsh
+set -eo pipefail
+source /home/synchronous/.agenix/agenix/zsh_remote
+restic -r sftp:${aliases[kubeserver]#ssh }:/home/synchronous/backups/journal \
+    --password-file /home/synchronous/.agenix/agenix/restic-password \
+    backup /home/synchronous/journal
 
-# soon: other backup locations...
-# restic -r sftp:${aliases[kubeserver]#ssh }:/home/synchronous/backups/journal backup /home/synchronous/journal
 # restic -r sftp:${aliases[kubeserver]#ssh }:/home/synchronous/backups/journal backup /home/synchronous/journal
